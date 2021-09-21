23480aeb1532d44b3005a5cf0efe1311e6e978fe
chaotic-mo <motesraji@gmail.com>
2021-09-21 12:17:35 +0200

    'elif_statement'

--- meten_is_weten.py
+++ meten_is_weten.py
@@ -1,6 +1,10 @@
-a = input("voer een random getal in. ")
-b = input("voer nogmaals een random getal toe. ")
+a = input("voer een random getal in: ")
+b = input("voer nogmaals een random getal toe: ")
 
 if a > b:
     max = a
     print('a is het grootste getal ' + max)
+elif a < b:
+    min = a
+    print('a is het kleinste getal ' + min)
+    
